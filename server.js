16e9010a0617b73533de36887fe8f4925f08f46c
Adrian Steppat <adrian.steppat@outlook.de>
2018-04-26 12:17:25 +0200

    initial commit with basic server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/assets',express.static(__dirname + '/assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+server.listen(process.env.PORT || 8081,function(){
+    console.log('Listening on '+server.address().port);
+});
