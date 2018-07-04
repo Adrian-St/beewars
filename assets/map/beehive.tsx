8bd5163b8697eb0783c7390dd334c3411650e79f
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-07-05 00:48:40 +0200

    removed a file

--- assets/map/beehive.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Bee Hive" tilewidth="32" tileheight="32" tilecount="20" columns="4">
- <image source="../sprites/beehive.png" width="128" height="160"/>
-</tileset>
